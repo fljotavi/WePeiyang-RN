66a03a9068c62adf65e9df1f8e2cb4da2ea1f0f2
Tzingtao Chow <Cyphexl@outlook.com>
2019-07-28 09:25:46 +0800

    Async-ify-preData-process

--- app/screens/home-screen/home-screen.tsx
+++ app/screens/home-screen/home-screen.tsx
@@ -19,6 +19,8 @@ import { CourseDailySchedule } from "../../components/course-daily-schedule"
 import { BookList } from "../../components/book-list"
 
 import { format } from "date-fns"
+import Toast from "react-native-root-toast";
+import toastOptions from "../../theme/toast";
 
 export interface HomeScreenProps extends NavigationScreenProps<{}> {
 
@@ -44,16 +46,22 @@ class HomeScreen extends React.Component<HomeScreenProps, {}> {
   state = {
     refreshing: false,
   }
-  prepareData = () => {
-    this.props.fetchUserData()
-    this.props.fetchCourseData()
-    this.props.fetchLibraryData()
-    this.props.fetchGpaData()
+
+  prepareData = async () => {
+    await Promise.all([
+      this.props.fetchUserData(),
+      this.props.fetchCourseData(),
+      this.props.fetchLibraryData(),
+      this.props.fetchGpaData()
+    ]).then(function(values) {
+      Toast.show(<Text tx="homeScreen.prepareDataSuccess" style={{ color: toastOptions.primary.textColor }}/> as any, toastOptions.primary)
+      console.log(values)
+    })
   }
 
   _onRefresh = () => {
     this.setState({ refreshing: true })
-    this.props.fetchGpaData().then(() => {
+    this.prepareData().then(() => {
       this.setState({ refreshing: false })
     })
   }
@@ -77,64 +85,66 @@ class HomeScreen extends React.Component<HomeScreenProps, {}> {
     )
 
     return (
-      <ScrollView refreshControl={
-        <RefreshControl
-          refreshing={this.state.refreshing}
-          onRefresh={this._onRefresh}
-        />
-      } >
-        <View style={ss.container}>
-          <View style={ss.headerBar}>
-            <Text text="Hello" preset="h2"/>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('user')}>
-              <View style={ss.userInfo}>
-                <Text text={userData.data.twtuname} style={ss.userName}/>
-                <Text text="  "/>
-                <Image source={{ uri: userData.data.avatar }} style={ss.avatar}/>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <ScrollView style={ss.horiScrollSelf} contentContainerStyle={ss.horiScroll} horizontal={true} showsHorizontalScrollIndicator={false}>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.bike" icon="directions_bike"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.contact" icon="call"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.learning" icon="assignment_turned_in"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.library" icon="local_library"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.cards" icon="credit_card"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.gpa" icon="timeline"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.classroom" icon="room"/>
-            <ModuleButton style={ss.blockWithMarginRight} tx="modules.coffee" icon="free_breakfast"/>
-            <ModuleButton tx="modules.buses" icon="directions_bus"/>
-          </ScrollView>
-          <View style={ss.sectionHead}>
-            <Text text={formattedHead} preset="h5"/>
-          </View>
-          <CourseDailySchedule
-            data={courseData.data}
-            status={courseData.status}
-            timestamp={timestamp}
+      <Screen>
+        <ScrollView refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
           />
-          <View style={ss.sectionHead}>
-            <Text text="Library" preset="h5"/>
+        } >
+          <View style={ss.container}>
+            <View style={ss.headerBar}>
+              <Text text="Hello" preset="h2"/>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate('user')}>
+                <View style={ss.userInfo}>
+                  <Text text={userData.data.twtuname} style={ss.userName}/>
+                  <Text text="  "/>
+                  <Image source={{ uri: userData.data.avatar }} style={ss.avatar}/>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <ScrollView style={ss.horiScrollSelf} contentContainerStyle={ss.horiScroll} horizontal={true} showsHorizontalScrollIndicator={false}>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.bike" icon="directions_bike"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.contact" icon="call"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.learning" icon="assignment_turned_in"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.library" icon="local_library"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.cards" icon="credit_card"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.gpa" icon="timeline"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.classroom" icon="room"/>
+              <ModuleButton style={ss.blockWithMarginRight} tx="modules.coffee" icon="free_breakfast"/>
+              <ModuleButton tx="modules.buses" icon="directions_bus"/>
+            </ScrollView>
+            <View style={ss.sectionHead}>
+              <Text text={formattedHead} preset="h5"/>
+            </View>
+            <CourseDailySchedule
+              data={courseData.data}
+              status={courseData.status}
+              timestamp={timestamp}
+            />
+            <View style={ss.sectionHead}>
+              <Text text="Library" preset="h5"/>
+            </View>
+            <BookList data={libraryData.data} status={libraryData.status} />
+            <View style={ss.sectionHead}>
+              <Text text="GPA Curve" preset="h5"/>
+            </View>
+            <GpaCurve
+              data={gpaData.data.gpaSemestral[scoreType]}
+              status={gpaData.status}
+              style={ss.curveView}
+              scoreToFixed={digitsFromScoreType(scoreType)}
+            />
+            <GpaStat
+              style={ss.stat}
+              status={gpaData.status}
+              setScoreType={(scoreType) => setScoreType(scoreType)}
+              scores={gpaData.data.gpaOverall}
+            />
+            <Button style={ss.moreButton} tx="homeScreen.more"/>
           </View>
-          <BookList data={libraryData.data} status={libraryData.status} />
-          <View style={ss.sectionHead}>
-            <Text text="GPA Curve" preset="h5"/>
-          </View>
-          <GpaCurve
-            data={gpaData.data.gpaSemestral[scoreType]}
-            status={gpaData.status}
-            style={ss.curveView}
-            scoreToFixed={digitsFromScoreType(scoreType)}
-          />
-          <GpaStat
-            style={ss.stat}
-            status={gpaData.status}
-            setScoreType={(scoreType) => setScoreType(scoreType)}
-            scores={gpaData.data.gpaOverall}
-          />
-          <Button style={ss.moreButton} tx="homeScreen.more"/>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </Screen>
     )
   }
 }
