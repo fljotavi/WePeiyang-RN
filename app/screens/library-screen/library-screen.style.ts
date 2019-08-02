49884065c3c3d6c585bf62fa9eb9875fddcc9366
Tzingtao Chow <Cyphexl@outlook.com>
2019-08-02 23:03:28 +0800

    Headers

--- app/screens/library-screen/library-screen.style.ts
+++ app/screens/library-screen/library-screen.style.ts
@@ -1,7 +1,29 @@
-import { color } from "../../theme"
+import { color, layoutParam } from "../../theme"
+import { TextStyle, ViewStyle } from "react-native"
 
 export default {
   screen: {
     backgroundColor: color.primary,
-  },
+  } as ViewStyle,
+  container: {
+    paddingHorizontal: layoutParam.paddingHorizontal,
+    paddingVertical: layoutParam.paddingVertical,
+    alignItems: "center",
+  } as ViewStyle,
+  heading: {
+    alignSelf: "flex-start",
+  } as ViewStyle,
+  headingKey: {
+    color: color.background,
+    fontSize: 20,
+    textTransform: "uppercase",
+    letterSpacing: 4,
+  } as TextStyle,
+  headingValue: {
+    color: color.background,
+    fontSize: 100,
+    fontWeight: 'bold',
+    marginTop: -20,
+    marginBottom: 20
+  } as TextStyle,
 }
--- app/screens/library-screen/library-screen.tsx
+++ app/screens/library-screen/library-screen.tsx
@@ -5,7 +5,8 @@ import { Screen } from "../../components/screen"
 
 import ss from "./library-screen.style"
 import { NavigationScreenProps } from "react-navigation"
-import { StatusBar } from "react-native"
+import { StatusBar, View } from "react-native"
+import { Text } from "../../components/text"
 
 export interface LibraryScreenProps extends NavigationScreenProps<{}> {
 
@@ -23,6 +24,17 @@ export class LibraryScreen extends React.Component<LibraryScreenProps, {}> {
           backgroundColor="transparent"
           barStyle='light-content'
         />
+
+        <View style={ss.container}>
+          <View style={ss.heading}>
+
+            <Text text="Books Borrowed" style={ss.headingKey}/>
+            <Text text="2" style={ss.headingValue}/>
+            <Text text="Quota" style={ss.headingKey}/>
+            <Text text="10" style={ss.headingValue}/>
+
+          </View>
+        </View>
 
       </Screen>
     )
